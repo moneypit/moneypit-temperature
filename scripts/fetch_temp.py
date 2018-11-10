918b113c03c388ca55b5d630239717e074305083
jobrieniii <jobrieniii@yahoo.com>
2018-11-10 09:16:28 +0000

    update to handle if sensor doesnt respond

--- scripts/fetch_temp.py
+++ scripts/fetch_temp.py
@@ -12,6 +12,7 @@ with open(sys.argv[1]) as f:
 rClient = redis.Redis(config['redis']['host'],config['redis']['port'])
 
 temp_sensors = config['temp']
+print temp_sensors;
 
 temperatureObject = {}
 
@@ -24,9 +25,17 @@ for i, temp_sensor in enumerate(temp_sensors):
     pin = temp_sensor['pin']
 
     temperatureObject[sensor_name] = {}
-
-    temperatureObject[sensor_name]['humidity'], temperatureObject[sensor_name]['temperatureC'] = Adafruit_DHT.read_retry(sensor, pin)
-    temperatureObject[sensor_name]['temperatureF'] = (temperatureObject[sensor_name]['temperatureC'] * 1.8) + 32
+    
+    print Adafruit_DHT.read(sensor, pin)
+   
+    temperatureObject[sensor_name]['humidity'], temperatureObject[sensor_name]['temperatureC'] = Adafruit_DHT.read(sensor, pin)
+    
+    if temperatureObject[sensor_name]['temperatureC'] is None:
+    	temperatureObject[sensor_name]['humidity'] = 0.0
+        temperatureObject[sensor_name]['temperatureC'] = 0.0
+        temperatureObject[sensor_name]['temperatureF'] = 0.0
+    else:
+        temperatureObject[sensor_name]['temperatureF'] = (temperatureObject[sensor_name]['temperatureC'] * 1.8) + 32
 
 
 print temperatureObject
